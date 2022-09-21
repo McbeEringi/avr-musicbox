c1c14b3856de9197825856cc4a4d440ebd1be5f4
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-09-21 23:35:55 +0900

    -

--- music_box/music_box.ino
+++ music_box/music_box.ino
@@ -2,15 +2,15 @@ void setup(){
   DDRB=B001000;//set PIN3 to output
   PORTB=B001000;//set ZZHZZZ
 
-  OCR1C=146;//MAX
-  OCR1B=73;//threshold
+  OCR1C=143;//MAX
+  OCR1B=0;//threshold
 
   //タイマー1制御レジスタ2
   // CTC1 PWM1A COM1A1 COM1A0 CS13 CCS12 CS11 CS10
   //-----
   // 0    CTC PWM使うので無関係
   // 000  PWM1A使わん
-  // 1001 16500k/CS/OCR1C=Hz
+  // 1001 (PCK||CK)/CS/OCR1C=Hz
   TCCR1=B00001001;
 
   //一般タイマ―制御レジスタ という名の タイマー1制御レジスタ サブ
@@ -31,10 +31,17 @@ void setup(){
   // -   予約
   //TIMSK=B00000100;
 
+  //PLL制御状態レジスタ
+  // LSM - - - - PCKE PLLE PLOCK
+  // 0    低速動作はしない
+  // 0000 予約
+  // 111  PCK, PLL Plock許可 64MHz動作
+  PLLCSR=B00000111;
+
   TCNT1=0;//timer1 reset
 }
 void loop(){
-  OCR1B=millis()%1000<50?73:OCR1C;
+  OCR1B=millis()%1000<50?72:OCR1C;//1760Hz
 }
 /*ISR(TIMER1_OVF_vect){
   OCR1B=millis()%1000<50?73:OCR1C;
