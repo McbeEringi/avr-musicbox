3e4e3fd7b59ef15263a5821190820ddc7b98c6d7
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-05-03 21:05:28 +0900

    -

--- t202/src/main.c
+++ t202/src/main.c
@@ -1,4 +1,5 @@
 #define F_CPU 20000000
+#define F_WAV 40000
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
@@ -6,13 +7,13 @@
 #define LED_BUILTIN 3
 #define BTN_BUILTIN 7
 
-#define WAIT_BTN while(BTN_DOWN);WAIT_5
+#define WAIT_BTN while(BTN_DOWN)WAIT_5
 #define BTN_DOWN ~VPORTA.IN&(1<<7)
 #define WAIT_5 for(uint8_t i=0;i<200;i++)WAIT// 5ms
 #define WAIT do{while(!(TCB0.INTFLAGS&TCB_CAPT_bm));TCB0.INTFLAGS=TCB_CAPT_bm;}while(0)// TCB0待ち(25us) 解除
 
-void sleep(){sei();sleep_cpu();cli();WAIT_BTN;}// ピン割り込みはBOTHEDGESかLEVELじゃなきゃ起きない
-ISR(PORTA_PORT_vect){PORTA.INTFLAGS=PORT_INT7_bm;}
+void sleep(){sei();SLPCTRL.CTRLA=SLPCTRL_SMODE_PDOWN_gc|SLPCTRL_SEN_bm;sleep_cpu();cli();WAIT_BTN;}// avr/sleep.hが仕事しないので手動設定
+ISR(PORTA_PORT_vect){PORTA.INTFLAGS=PORT_INT7_bm;}// ピン割り込みはBOTHEDGESかLEVELじゃなきゃ起きない リセット必須
 
 void blink(uint8_t x){// 下位bitから読み込み MSBの状態のまま離脱
 	for(uint8_t i=0;i<8;i++){
@@ -21,7 +22,23 @@ void blink(uint8_t x){// 下位bitから読み込み MSBの状態のまま離脱
 	}
 }
 
-void play(const uint8_t *w){
+void play(const uint8_t *s){
+	const uint8_t
+		*p[MTRKS],*_p[MTRKS];// チャネルポインタ ループ用に二重保存
+	uint16_t
+		x[MTRKS],_x,// PROGMEMから読んだ音符とその合計
+		t[MTRKS],_t[MTRKS],// 音高とその波形生成用カウンタ
+		dt,_dt,// 最小音価とそのカウンタ
+		h=pgm_read_word_near(s++);// ヘッダー
+	uint8_t
+		v[MTRKS],// 音量
+		l[MTRKS],// 音価
+		decay,// 減衰カウンタ
+		ntrks=0;// 有効トラック数
+	bool
+		w[MTRKS],// 波形
+		d[MTRKS];// 減衰
+
 
 }
 
@@ -36,13 +53,13 @@ void main(){
 	// TCB0 波形生成 これが解像度 可聴域上端の2倍弱あれば何とかなる 20MHz/500=40kHz
 	TCB0.CTRLA=TCB_CLKSEL_CLKTCA_gc|TCB_ENABLE_bm;// TCAとクロック共有 TCB有効
 	TCB0.CTRLB=TCB_CNTMODE_INT_gc;// 周期的割り込み動作
-	TCB0.CCMP=499;// TOP
+	TCB0.CCMP=F_CPU/F_WAV-1;// TOP
 
 	_PROTECTED_WRITE(CLKCTRL.MCLKCTRLB,0);// 分周無効化 CPUも周辺機能も20MHz
 	PORTA.DIRSET=0b1100;// 出力: PA3,2
 	PORTA.PIN7CTRL=PORT_PULLUPEN_bm|PORT_ISC_LEVEL_gc;// PA7 プルアップ LOWで割り込み 
-	SLPCTRL.CTRLA=SLPCTRL_SMODE_PDOWN_gc|SLPCTRL_SEN_bm;// avr/sleep.hが仕事しないので
+	
 	while(1){
-		blink(0b01001001);sleep();
+		blink(0b01111111);sleep();
 	}
 }
--- t202/src/seq.h
+++ t202/src/seq.h
@@ -3,21 +3,24 @@
 /*
 sequence
 	header
-
+		TTTTTL__
+			tickPerSec = T;// BPM:120,minNote:16=tps:8
+			loop? = L
 	track...
-		0: track start
-		LLLNNNNN...: note data
-			L=
-			N=1(F#3)~16(A4)~31(C6), 0(Rest)
-	0,0: end of seq
+		WWWWWWWW
+			wave form = W ( 2bit*4 )
+		DDOOE_SS
+		SSSSSSSS
+			envelope? = E
+			note data size(byte) = S
+		LLLNNNNN... : note data
+			length = [1,2,3,4,6,8,12,16][L]<<D
+			note = [ 1(F#3)~16(A4)~31(C6) ][N]+O*12
+	0 : end of seq
 */
 static const uint8_t yobikomi[]={
-	255,
-	0,
-	0,
-	0,
-	0,
-	0,0
 };
 
 #endif
+
+
