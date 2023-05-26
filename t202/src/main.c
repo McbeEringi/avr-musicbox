aa7401ddeabe578641306697340f2446359852c1
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-05-26 19:24:53 +0900

    -

--- t202/src/main.c
+++ t202/src/main.c
@@ -13,7 +13,7 @@
 
 const uint16_t t0[]={2446,2309,2179,2057,1942,1833,1730,1633,1541,1455,1373,1296};// C0~B0
 
-void sleep(){sei();SLPCTRL.CTRLA=SLPCTRL_SMODE_PDOWN_gc|SLPCTRL_SEN_bm;sleep_cpu();cli();WAIT_BTN;}// avr/sleep.hが仕事しないので手動設定
+void sleep(){uint8_t d=VPORTA.DIR;VPORTA.DIR=0;sei();SLPCTRL.CTRLA=SLPCTRL_SMODE_PDOWN_gc|SLPCTRL_SEN_bm;sleep_cpu();cli();WAIT_BTN;VPORTA.DIR=d;}// avr/sleep.hが仕事しないので手動設定
 ISR(PORTA_PORT_vect){PORTA.INTFLAGS=PORT_INT7_bm;}// ピン割り込みはBOTHEDGESかLEVELじゃなきゃ起きない リセット必須
 
 void blink(uint8_t x){// 下位bitから読み込み MSBの状態のまま離脱
@@ -23,13 +23,25 @@ void blink(uint8_t x){// 下位bitから読み込み MSBの状態のまま離脱
 	}
 }
 
+void play(){
+	uint16_t t[3]={76,61,51},_t[3]={0,0,0};//C5,E5,G5
+	for(uint16_t i=0;i<-1;i++){
+		TCA0.SINGLE.CMP2=
+		((_t[0]=(++_t[0]==t[0]?0:_t[0]))<(t[0]>>1)?255>>2:0)+
+		((_t[1]=(++_t[1]==t[1]?0:_t[1]))<(t[1]>>1)?255>>2:0)+
+		((_t[2]=(++_t[2]==t[2]?0:_t[2]))<(t[2]>>1)?255>>2:0)
+		;
+		WAIT;
+	}
+}
+
 
 void main(){
 	// TCA0 疑似DAC 可能な限り高速な方が良い 20MHz/(8bit=2**8)=78.125kHz
 	TCA0.SINGLE.CTRLA=TCA_SINGLE_ENABLE_bm;// TCA有効
 	TCA0.SINGLE.CTRLB=TCA_SINGLE_CMP2EN_bm|TCA_SINGLE_WGMODE_SINGLESLOPE_gc;// TCA0 wo2有効 単傾斜PWM
-	TCA0.SINGLE.PER=45454;// TOP 8bit
-	TCA0.SINGLE.CMP2=22727;// clear
+	TCA0.SINGLE.PER=0xff;//45454;// TOP 8bit
+	//TCA0.SINGLE.CMP2=22727;// clear
 
 	// TCB0 波形生成 これが解像度 可聴域上端の2倍弱あれば何とかなる 20MHz/500=40kHz
 	TCB0.CTRLA=TCB_CLKSEL_CLKTCA_gc|TCB_ENABLE_bm;// TCAとクロック共有 TCB有効
@@ -41,6 +53,6 @@ void main(){
 	PORTA.PIN7CTRL=PORT_PULLUPEN_bm|PORT_ISC_LEVEL_gc;// PA7 プルアップ LOWで割り込み 
 	
 	while(1){
-		blink(0b00110011);sleep();
+		blink(0b00110011);sleep();blink(0b01010101);play();//play(yobikomi);
 	}
 }
--- t202/src/seq.h
+++ t202/src/seq.h
@@ -1,8 +1,8 @@
 #ifndef SEQ_H
 #define SEQ_H
 
-static const uint8_t yobikomi[]={
-};
+// static const uint8_t yobikomi[]={
+// };
 
 #endif
 
