da37ecdc0ac46f63efec837bca7a3aed176c9e0e
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-06-20 02:51:14 +0900

    -

---
+++ sample/auld_lang_syne.js
@@ -0,0 +1,25 @@
+({
+	title:'auld_lang_syne',
+	tpm:100*8/4,
+	loop:true,
+	notes:`
+		x0000 2A>T4DtDTDtgTftDTf1gf4DtDTgtA>TctrTr
+		tc<TAtgTgtDTftDTf1gf4DtcTc<tA>TDtrTr>
+		tc<TAtgTgtDTftDTf>tc<TAtgTgtA>TctrTr
+		tc<TAtgTgtDTftDTf1gf4DtcTc<tA>TDtrTr<
+		
+		x0010 tA>TDtDTDtgTftDTf1gf4DtDTgtA>TctrTr
+		x0000 tc<TAtgTgtDTftDTf1gf4DtcTc<tA>TDtrTr>
+		x0010 tc<TAtgTgtDTftDTf>tc<TAtgTgtA>TctrTr
+		tD<TAtgTgtDTftDTf x0000 1gf4DtcTc<tA>TDtrTr
+		
+		x0010 tDTGtGTG>tc<TAtGTA>1c<A4GtG>TctDTftrTr
+		tfTDtcTc<tGTAtGTA>1c<A4GtfTftDTGtrTr>
+		x0000 tfTDtcTc<tGTAtGTA>tfTDtcTctDTftrTr
+		x0010 tfTDtcTc<tGTAtGTA>1c<A4GtfTftDTGtrTr>
+		x0000 tfTDtcTc<tGTAtGTA>tfTDtcTctDTftrTr
+		tGTDtcTc<tGTAtGTA>1c<A4GtfTftDTGtrTr
+		
+		trTrtr
+	`
+})
