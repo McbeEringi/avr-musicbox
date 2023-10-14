b17fd05004969fa25e50e452bac072ffa4add597
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-10-15 03:33:58 +0900

    -

---
+++ sample/small_world.js
@@ -0,0 +1,50 @@
+({
+	tpm:130*16/4,
+	title:'small_world',
+  loop:true,
+	notes:`
+		x0010 2b>c
+		T4dbgtagTgFF<tab>
+		TcaFtgFTedd<tb>c
+		TdtgaTbtagTetab>Tc<tba
+		Td>c<ba8gr
+
+		6gtgT4bg6ataT8a
+		6ataT4>c<a6btbT8b
+		6btbT4>d<b>6ctcT4ct<ba
+		8dFgTr,
+
+		x1110 4r2>>
+		rbrbrbrb>rcrcrcrc
+		rcrcrcrc<rbrbrbrb
+		rbrb>rdrdrererere
+		rdrdrcrc<rbrbbrr>c<
+
+		rbrbrbrb>rcrcrcrc
+		rcrcrcrc<rbrbrbrb
+		rbrb>rdrdrererere
+		rdrdrcrc<rbrbbr,
+
+		x1100 4r2>>
+		rgrgrgrgrararara
+		rarararargrgrgrg
+		rgrgrbrb>rcrcrCrC
+		rcrc<rarargrggrra
+
+		rgrgrgrgrararara
+		rarararargrgrgrg
+		rgrgrbrb>rcrcrCrC
+		rcrc<rarargrggr,
+
+		x1000 4r>
+		gdgdadad
+		adadgdgd
+		ggbb>ccCC
+		d<dFagdgd
+
+		gdgdadad
+		adadgdgd
+		ggbb>ccCC
+		d<dFagdg
+	`
+})
