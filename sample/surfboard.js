9e58e86a258243b049681e82cee6035acc11bab7
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2024-04-05 23:54:12 +0900

    -

---
+++ sample/surfboard.js
@@ -0,0 +1,18 @@
+({
+	title:'surfboard',
+	tpm:100*16/4,
+	loop:true,
+	notes:`
+		x1010 >>>1
+		aarggrFFreerDDrddrDDreerffrgg rggrffreerddrCCrccrCCrddrDDrffr
+		ffrDDrddrccr<AAraar>ffrDDrddrccr<AAraar> 2Dd1Df4F1fD 2Dd1Df4F2r
+		,
+		x1000 >4
+		gdgdgdcg f>c<f>c<f>c<Af
+		DADAdada 6gGgG
+		,
+		x0110 >>>1
+		CCbCCbCCbCbCCbCC bCCbCCbCCbCbbCbC CCbCCbCCbCCbCCbC CbCCbCCbCCbCCbCC
+		CCbCCbCCbCbCCbCC bCCbCCbCCbCbbCbC 12rr
+	`
+})
