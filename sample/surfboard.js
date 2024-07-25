22a5417461625d6db6b3b39829b0f90093a35d00
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2024-07-25 18:08:50 +0900

    -

--- sample/surfboard.js
+++ sample/surfboard.js
@@ -1,18 +1,98 @@
 ({
 	title:'surfboard',
-	tpm:100*16/4,
+	tpm:101*48/4,
 	loop:true,
+	//wav:'mml_recorded',// enablie this line to automaticly record and download
 	notes:`
-		x1010 >>>1
-		aarggrFFreerDDrddrDDreerffrgg rggrffreerddrCCrccrCCrddrDDrffr
-		ffrDDrddrccr<AAraar>ffrDDrddrccr<AAraar> 2Dd1Df4F1fD 2Dd1Df4F2r
+		x1010 >>>3
+		aarggrFFreerDDrd drDDreerffrggrgg
+		rffreerddrCCrccr CCrddrDDrffrffrD
+		Drddrccr<bbrAAr>ff rDDrddrccr<bbrAAr>
+		8Dd4Df16F4fD 8Dd4Df16F8r3
+
+		aarggrFFreerDDrd drDDreerffrggrgg
+		rffreerddrCCrccr CCrddrDDrffrffrD
+		Drddrccr<bbrAAr>ff rDDrddrccr<bbrAAr>
+		8Dd4Df16F4fD 8Dd4Df16F8r
+
+		8f<b>f<b>f<b> f<b>f<b>x1110f<b>
+		f<b>f<b>f<b> f<b>f<b>f<b>
+		f<b>f<b>f<b> f<b>f<b>f<b>
+		f<b>f<b>f<b> f<b>f<b>f<b>
+		
+		x0011 <<
+		3agragragragragra gagraGgF12gr
+		3gfrgfrgfrgfrgfrg fgfrgFfe12fr
+		3fDrfArfDrf<A>rfDrf >c<rfDrf14c4rx1010>>rfD
+		8Dd4Df16F4fD 8Dd4Df16F8r
 		,
-		x1000 >4
-		gdgdgdcg f>c<f>c<f>c<Af
-		DADAdada 6gGgG
+
+		
+		x1110 >>>
+		3ddrddrddrddrddrc crCCrddrddrCCrcc
+		rccrccrccr<bbrAAr bbr>ccrccrccrccr<A
+		ArAArAArAAraaraa raaraaraaraaraar
+		24rr rr>
+
+		3ddrddrddrddrddrc crCCrddrddrCCrcc
+		rccrccrccr<bbrAAr bbr>ccrccrccrccr<A
+		ArAArAArAAraaraa raaraaraaraaraar
+		24aA aA
+		
+		a <<x0110 1abababababababababababab abababababababababababab abababx0100aAb>cCdDefFgGaAb>cCd<<
+		x0000
+		>>6r12D6C<AbGD 4CD<b18A6G3gfDC
+		x1000>>4baG32g x0110 1<eded edededededededededededed ededex0100defFgGaAb>cCdDefFgGa
+		x00006r12A6GDC<Ab gGDC3c<bAG4gfD
+		
+		x01103grrgrrrgrgrgrggr T6gtgrggrgrgrgrggr
+		arrarararrraarar TFtFrrFrFrFrT12Ftr
+		rgrgrggrT6gtrgrgrg rfrfrrffrfrfrT9f
+		24aA aA
 		,
-		x0110 >>>1
-		CCbCCbCCbCbCCbCC bCCbCCbCCbCbbCbC CCbCCbCCbCCbCCbC CbCCbCCbCCbCCbCC
-		CCbCCbCCbCbCCbCC bCCbCCbCCbCbbCbC 12rr
+		
+		
+		x1000 >9
+		tgT3gtdTdtgTgtdTd tgTgtdTdtcTctgTg
+		tfTf>tcTc<tfTf>tcTc< tfTf>tcTc<tATAtfTf
+		tDTDtATAtDTDtATA tdTdtaTatdTdtaTa
+		24gG gG
+
+		tgT3gtdTdtgTgtdTd tgTgtdTdtcTctgTg
+		tfTf>tcTc<tfTf>tcTc< tfTf>tcTc<tATAtfTf
+		tDTDtATAtDTDtATA tdTdtaTatdTdtaTa
+		24gG gG
+
+		gr rrT3>
+		tCTC<tGTG>tCTC<tGTG> tCTC<tGTG>tCTC<tGTG
+		24gr rrT3>
+		tCTC<tGTG>tCTC<tGTG> tCTC<tGTG>tCTC<tGTG
+		
+		tgTgtdTdtgTgtdTd tCTCtFTFtCTCtFTF
+		tcTctfTftcTctfTf <tbTb>teTe<tbTb>teTe
+		<tATA>tDTD<tATA>tDTD tdTdtaTatdTdtaTa
+		24gG gG
+		,
+
+		
+		x0110 >>>3
+		CCbCCbCCbCbCCbCC bCCbCCbCCbCbbCbC
+		CCbCCbCCbCCbCCbC CbCCbCCbCCbCCbCC
+		bCCbCCbCCbCCbCCb CCbCCCbCCbCCbCbC
+		T24rr rrt
+
+		CCbCCbCCbCbCCbCC bCCbCCbCCbCbbCbC
+		CCbCCbCCbCCbCCbC CbCCbCCbCCbCCbCC
+		bCCbCCbCCbCCbCCb CCbCCCbCCbCCbCbC
+		T24rr rr
+
+		rr rrt
+		CCbCCbCCbCbCCbCC CCbCCbCbCbCbbrrr
+		Trr rrt
+		rrrrrrrrrrrrrrrr rrrrrrrrrrrrrrrr
+		
+		rrrrrrrrrrrrrrrr rrrrrrrrrrrrrrrr
+		rrrrrrrrrrrrrrrr rrrrrrrrrrrrrrrr
+		rrrrrrrrrrrrrrrr rrrrrrrrrrrrrrrr
 	`
 })
