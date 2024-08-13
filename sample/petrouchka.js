ab1f35d0318c50a5e8abc7e97fd2b269bceb707c
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2024-08-13 14:01:35 +0900

    -

---
+++ sample/petrouchka.js
@@ -0,0 +1,14 @@
+({
+	title:'petrouchka',
+	tpm:130*16/4,
+	loop:true,
+	notes:`
+		x0000>
+		6f2g6G2f 2GGgf4gc 6g2G6A2g 2AAGgfc1fgGA
+		>4c2f<C>4D2fD CCc<A>4c<f 2r4>C<A>c<2G gcGgfc1fgGA
+		>4c2f<C>4D2fD CCc<A>4c<f 2r4>C<A>c<2G A>cde4f<f,
+		x1000>>4f>cfG fceg e<A>Cg c2eg4G2fc
+		<f>cGf 1gfef2Cc <A>fcC<f>c1GfgG <4A>2Cf<4f>2Gf 4c2eg4G2fc
+		<f>cGf 1gfef2Cc <A>fcC<f>c1GfgG <4A>2Cf<4f>2Gf 4g2ec<8f
+	`
+})
