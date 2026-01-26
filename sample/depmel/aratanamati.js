1aa4800e43ebdeb22b3de915712045b0238dd034
McbeEringi <git@mcbeeringi.dev>
2026-01-26 18:21:43 +0900

    Create aratanamati.js

---
+++ sample/depmel/aratanamati.js
@@ -0,0 +1,13 @@
+({
+	title:'aratanamati',
+	tpm:140*8/4,
+	loop:false,
+	//wav:'mml_recorded',// enablie this line to automaticly record and download
+	notes:`
+		x0000>1
+		1G2ab1e2G
+		1a2b>4C<1b
+		a2b>C1D<2b>
+		4F
+	`
+})
