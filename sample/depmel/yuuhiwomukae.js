8e879faba5791a3e48666998e055b89c2bfb1c9f
McbeEringi <git@mcbeeringi.dev>
2026-01-26 18:21:10 +0900

    Create yuuhiwomukae.js

---
+++ sample/depmel/yuuhiwomukae.js
@@ -0,0 +1,16 @@
+({
+	title:'yuuhiwomukae',
+	tpm:180*8/4,
+	loop:false,
+	//wav:'mml_recorded',// enablie this line to automaticly record and download
+	notes:`
+		x0000>>1
+		C<CFAb<b>DF>
+		C<CFA>D<DFb>
+		C<CFAb<b>DF>
+		C<CFA>D<DFb>
+		C<CFAb<b>DF>
+		D<DFb>f<CG>C
+		4C
+	`
+})
