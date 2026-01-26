18d192f4fe53c8744bd64bb57b62f0399c2e6c83
DG-7D <96519093+DG-7D@users.noreply.github.com>
2026-01-26 18:32:34 +0900

    Add files via upload

---
+++ sample/mizuto_midorino_machi.js
@@ -0,0 +1,17 @@
+({
+	title:'mizuto_midorino_machi',
+	tpm:96*4,
+	loop:false,
+	// wav:'mml_recorded',// enablie this line to automaticly record and download
+	notes:`
+		x0000 <<<<4g
+		>8e4e2ee 12e4e 10f2e2d2c 12d2e2f 8g8G 12a2g2e 2d8d4c<2b >12c2c2c
+		4a2a2a2a2a2g2f 12g2c2c 2f2f6f2f2e2d 6e2f4g4c 4a4a6b2b >10c2c2c<2b 6a4a2b2>c14d <<2g2g
+		>6e2e2e4e10e 4f4g 8f6e2d 12c<2g2g >6e2e2e4e10e 4f4g 8f6e2d 12c
+		,
+		x0000 2r<<<<2g
+		>4c<g>c<g >c<b>c<b >d<a>c<f g>c<bg >c<g>e<b a>e<ae dfgg >c<g>cc
+		<f>c<f>c <ebae dagb >c<g>cc <faeG a>e<ag F>d<a>d <g>c<bg
+		>c<g>c<g eAa>C d<Ggg >c<g>cc c<eg>c <eA8a >d<g >4c<g>c
+	`
+})
---
+++ sample/sukidesu_kawasaki_ainomachi.js
@@ -0,0 +1,13 @@
+({
+	title:'sukidesu_kawasaki_ainomachi',
+	tpm:110*4,
+	loop:true,
+	// wav:'mml_recorded',// enablie this line to automaticly record and download
+	notes:`
+		x0000 >>2d
+		4A2a2g4d<2A>2c 4d1f2D1d6c2c 4a2g2f2D2D2d2c 2d2d2d2d6d2d
+		4A2a2g4d<2A>2c 4d2f1D1d6c2c 3D1D2d2c<2A>2c2d2g 6F2F6g1g1a
+		2b2b2g>2c<4b2g2d 4e4g6d1g1a 2b2b2g>2c<4b2g2b 4a4a6a1g1a
+		4b2g>2c<2b2b2g2d 4e2g2g6d2g 4g2F2e2d2d2g2b >3d<1F2F2F6g1r
+	`
+})
