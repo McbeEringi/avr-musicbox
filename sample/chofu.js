05dc683ba7dff6416a1b00ffce2782cea9684e0e
McbeEringi <git@mcbeeringi.dev>
2026-01-26 18:20:08 +0900

    Create chofu.js

---
+++ sample/chofu.js
@@ -0,0 +1,28 @@
+({
+	title:'chofu',
+	tpm:180*16/4,
+	loop:false,
+	//wav:'mml_recorded',// enablie this line to automaticly record and download
+	notes:`
+		x0000 2g>db>e<<F>da>d<<
+		c>dcg4ga
+		8b4agdedr
+		2r4b>2c<4b2ag8ar
+		6b>2c4d2c<b>6c2d6e
+		2<e6F2e4da8gr
+		3a1b2ag4Fd
+		3g1F2ga4br
+		2r4a2b>4ce
+		2r6d4Ce
+		8dr,
+		x1010 16rr>>
+		2g>dgdadbd<
+		2b>dgdadgd<
+		2eb>egbegb<
+		>2adcd<ad>dc<
+		2g>dgr<g>dfr<
+		2cg>cd4eg<
+		2da>de4F2dc<
+		2g>dga4br
+	`
+})
